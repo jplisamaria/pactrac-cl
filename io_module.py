e51fac77b9d8a1bed00f76434554b6ab2ac90f55
Lisa-Maria Mehta <jplisamaria@gmail.com>
2016-05-11 01:37:35 -0400

    Adds csv file input, and outputs results to both screen and logfile

--- io_module.py
+++ io_module.py
@@ -1,5 +1,6 @@
 from APIs_module import *
 from io_module import *
+import csv, os
 
 def get_carrier(selection):
   if selection == "1":
@@ -12,8 +13,36 @@ def get_carrier(selection):
 
 def print_data(id, carrier):
   tracking_data = api_call(id, carrier)
+  tracking_string = "\n"
   print ("%s package %s" %(carrier, id))
-  print ("-" * len("%s package %s" %(carrier, id)) )
+  tracking_string += ("%s package %s \n" %(carrier, id))
+  print ("-" * len("%s package %s \n" %(carrier, id)) )
+  tracking_string += (("-" * len("%s package %s" %(carrier, id)) ) + "\n")
   for detail in tracking_data:
     print ("+++ " + detail)
+    tracking_string += ("+++ " + detail + "\n")
   print ()
+  tracking_string += ("\n")
+  return tracking_string
+
+def process_csv(file):
+  input_csv = open(file, "r")
+  #need to add error handling if file not found
+  reader = csv.reader(input_csv)
+  os.system('clear')
+  output_log = open("tracking_log.txt","w")
+  #would like to add tracking_log.csv
+  for row in reader:
+    carrier = str(row[0])
+    id = str(row[1])
+    if carrier not in ["USPS", "UPS", "FedEx", "FEDEX", "Fedex"]:
+      print ("--> Invalid Carrier!!")
+      print ("Package %s not tracked." %(id))
+      print()
+      print()
+    else:
+      if carrier in ["FedEx", "FEDEX", "Fedex"]:
+        carrier = "FedEx"
+      output_log.write(print_data(id, carrier))
+      #print_data(id, carrier)
+      print()
--- io_script.py
+++ io_script.py
@@ -1,7 +1,9 @@
 from io_module import *
+import csv
 import os
 
 os.system('clear')
+print()
 print ("****** Welcome to PACTRAC-cl, your favorite package tracker. *****")
 print ()
 while True:
@@ -9,18 +11,26 @@ while True:
   print ("          Enter 1 for United States Postal Service")
   print ("          Enter 2 for UPS")
   print ("          Enter 3 for FedEx")
-  print ("          Select q to quit")
+  print ("          Enter f for csv file.")
+  print ("          Enter q to quit")
   print ()
   carrier_selection = str(input ("     What is your selection?  "))
   print ()
-  while carrier_selection not in ["1","2","3", "q","Q"]:
+  while carrier_selection not in ["1","2","3", "f", "F","q","Q"]:
       print ("------> Warning: Selection Invalid!!")
       print ()
-      carrier_selection = input ("     Please select 1 for USPS , 2 for UPS, 3 for FedEx, and q to quit :  ")
+      carrier_selection = input ("     Please select 1 for USPS , 2 for UPS, 3 for FedEx, f for csv file, or q to quit :  ")
       print ()
   if carrier_selection in ["Q", "q"]:
     print ("Thank you, come again.")
     print()
+    exit()
+  if carrier_selection in ["f", "F"]:
+    print ("Enter name of csv file.  Please note that each line must be in the form of ")
+    print ("                valid_carrier_name, tracking_number  ")
+    filename = str(input("Filename ?  "))
+    print()
+    process_csv(filename)
     exit()
   carrier = get_carrier(carrier_selection)
   print ("     You have selected %s as your carrier." %(carrier))
@@ -37,3 +47,6 @@ while True:
     print ("Thank you, come again.")
     print ()
     exit()
+  else:
+    os.system('clear')
+    print()
